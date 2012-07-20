30888e95885bcc228b168f1897e2f0a758529599
hengsin <hengsin@localhost>
2012-07-20 14:44:45 +0800

    IDEMPIERE-345 Deployment: Bundle p2 director app and product update script. Make it work for both swing client and server product.

---
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/dashboard/DPPerformance.java
@@ -0,0 +1,63 @@
+/******************************************************************************
+ * Copyright (C) 2008 Elaine Tan                                              *
+ * Copyright (C) 2008 Idalica Corporation
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *****************************************************************************/
+package org.adempiere.webui.dashboard;
+
+import org.adempiere.webui.apps.graph.WPAPanel;
+import org.zkoss.zk.au.out.AuScript;
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Page;
+import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zk.ui.util.Clients;
+
+/**
+ * Dashboard item: Performance Indicators
+ * @author Elaine
+ * @date November 20, 2008
+ */
+public class DPPerformance extends DashboardPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8878665031716441912L;
+
+	public DPPerformance()
+	{
+		super();
+		
+        WPAPanel paPanel = WPAPanel.get();
+        if (paPanel != null) 
+        {
+        	this.appendChild(paPanel);        	
+        }
+	}	
+
+	
+	@Override
+	public void onPageAttached(Page newpage, Page oldpage) {
+		super.onPageAttached(newpage, oldpage);
+		if (newpage != null) {
+			Events.echoEvent("onPostRender", this, null);
+		}
+	}
+
+	//adjust window height to match grid height
+	public void onPostRender() 
+	{
+		Component grid = this.getFirstChild().getFirstChild();
+		String script = "setTimeout(function() { var grid = jq('#" + grid.getUuid() + "');";
+		script = script + "grid.parent().height(grid.css('height'));}, 500);";
+		Clients.response(new AuScript(script));
+	}
+}
