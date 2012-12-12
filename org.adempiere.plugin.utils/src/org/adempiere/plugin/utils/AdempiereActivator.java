c7ec618592b7ce0507ff219aa1aad925978c0786
tbayen <tbayen@localhost>
2012-12-12 23:52:58 +0100

    Script did not work with actual plugin version. Enhancement for calling with VM args

---
+++ org.adempiere.plugin.utils/src/org/adempiere/plugin/utils/AdempiereActivator.java
@@ -0,0 +1,120 @@
+package org.adempiere.plugin.utils;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.adempiere.base.IDictionaryService;
+import org.adempiere.base.Service;
+import org.compiere.model.Query;
+import org.compiere.model.X_AD_Package_Imp;
+import org.compiere.util.Env;
+import org.compiere.util.Trx;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class AdempiereActivator implements BundleActivator {
+
+	protected final static Logger logger = Logger
+			.getLogger(AdempiereActivator.class.getName());
+	private BundleContext context;
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		this.context = context;
+		if (logger.isLoggable(Level.INFO))
+			logger.info(getName() + " " + getVersion() + " starting...");
+		installPackage();
+		start();
+		if (logger.isLoggable(Level.INFO))
+			logger.info(getName() + " " + getVersion() + " ready.");
+	}
+
+	public String getName() {
+		return context.getBundle().getSymbolicName();
+	}
+
+	public String getVersion() {
+		return (String) context.getBundle().getHeaders().get("Bundle-Version");
+	}
+
+	public String getDescription() {
+		return getName();
+	}
+
+	private void installPackage() {
+		String trxName = Trx.createTrxName();
+		try {
+			String where = "Name=? AND PK_Version=?";
+			Query q = new Query(Env.getCtx(), X_AD_Package_Imp.Table_Name,
+					where.toString(), trxName);
+			q.setParameters(new Object[] { getName(), getVersion() });
+			X_AD_Package_Imp pkg = q.first();
+			if (pkg == null) {
+				packIn(trxName);
+				install();
+				if (logger.isLoggable(Level.INFO))
+					logger.info(getName() + " " + getVersion() + " installed.");
+			} else {
+				if (logger.isLoggable(Level.INFO))
+					logger.info(getName() + " " + getVersion() + " was installed: "
+						+ pkg.getCreated());
+			}
+			Trx.get(trxName, false).commit();
+		} finally {
+			if (Trx.get(trxName, false) != null) {
+				Trx.get(trxName, false).close();
+			}
+		}
+	}
+
+	protected void packIn(String trxName) {
+		URL packout = context.getBundle().getEntry("/META-INF/2Pack.zip");
+		if (packout != null) {
+			IDictionaryService service = Service.locator().locate(IDictionaryService.class).getService();
+			try {
+				// copy the resource to a temporary file to process it with 2pack
+				InputStream stream = context.getBundle().getEntry("/META-INF/2Pack.zip").openStream();
+				File zipfile = File.createTempFile(getName(), ".zip");
+				FileOutputStream zipstream = new FileOutputStream(zipfile);
+			    byte[] buffer = new byte[1024];
+			    int read;
+			    while((read = stream.read(buffer)) != -1){
+			    	zipstream.write(buffer, 0, read);
+			    }
+			    // call 2pack
+				service.merge(context, zipfile);
+			} catch (Exception e) {
+				logger.log(Level.SEVERE, "Error on Dictionary service", e);
+			}
+		} 
+	}
+
+	protected BundleContext getContext() {
+		return context;
+	}
+
+	protected void setContext(BundleContext context) {
+		this.context = context;
+	}
+	
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		stop();
+		logger.info(context.getBundle().getSymbolicName() + " "
+				+ context.getBundle().getHeaders().get("Bundle-Version")
+				+ " stopped.");
+	}
+
+	protected void install() {
+	};
+
+	protected void start() {
+	};
+
+	protected void stop() {
+	};
+}
---
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/AdempiereIdGenerator.java
@@ -0,0 +1,109 @@
+/******************************************************************************
+ * Copyright (C) 2010 Carlos Ruiz                                             *
+ * Copyright (C) 2009 Quality Systems & Solutions - globalqss                 *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *****************************************************************************/
+
+package org.adempiere.webui;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.Desktop;
+import org.zkoss.zk.ui.IdSpace;
+import org.zkoss.zk.ui.Page;
+import org.zkoss.zk.ui.metainfo.ComponentInfo;
+import org.zkoss.zk.ui.sys.IdGenerator;
+
+/**
+ * Id generator for selenium ide recording. 
+ * You don't need this to run ztl or zk jq selector based test script but if would help to 
+ * code or troubleshoot the test script. 
+ * 
+ * DON'T use this for other purpose, you have been warned :)
+ * 
+ * @author Carlos Ruiz
+ * @author hengsin
+ *
+ */
+public class AdempiereIdGenerator implements IdGenerator {
+
+	private static final String DEFAULT_ZK_COMP_PREFIX = "zk_comp_";
+	private static final String DESKTOP_ID_ATTRIBUTE = "org.adempiere.comp.id";
+	
+	@Override
+	public String nextComponentUuid(Desktop desktop, Component comp, ComponentInfo compInfo) {
+		String prefix = comp.getId(); 
+					
+		if (prefix == null || prefix.length() == 0) {
+			prefix = DEFAULT_ZK_COMP_PREFIX;
+		}
+		
+		StringBuilder builder = new StringBuilder(prefix);
+		Component parent = comp.getParent();
+		while(parent != null) {
+			//only include id space owner to ease converting test case to use zk id selector instead of uuid
+			if (parent instanceof IdSpace) {
+				String id = parent.getId();
+				if (id != null && id.length() > 0)
+					builder.insert(0, id+"_");				
+			}
+			parent = parent.getParent();
+		}
+		prefix = builder.toString();
+		
+		Pattern pattern = Pattern.compile("[^a-zA-Z_0-9]");
+		Matcher matcher = pattern.matcher(prefix);
+		StringBuffer sb = new StringBuffer();
+		while(matcher.find()) {
+			matcher.appendReplacement(sb, "_");
+		}
+		matcher.appendTail(sb);
+		prefix = sb.toString();
+		
+		if (desktop.getComponentByUuidIfAny(prefix) == null) { // look to avoid dups
+			return prefix;
+		} 
+
+		/* add sequence suffix to guarantee uniqueness */
+		int i = 0;
+		try {
+			String number = null;
+			if (desktop.getAttribute(DESKTOP_ID_ATTRIBUTE) != null) {
+				number = desktop.getAttribute(DESKTOP_ID_ATTRIBUTE).toString();
+				i = Integer.parseInt(number);
+				i++;// Start from 1
+			}
+		} catch (Throwable t) {
+			i = 1;
+		}
+		desktop.setAttribute(DESKTOP_ID_ATTRIBUTE, String.valueOf(i));
+		if (!prefix.endsWith("_"))
+			prefix = prefix + "_";
+		return prefix + i;
+	}
+
+	@Override
+	public String nextDesktopId(Desktop desktop) {
+		if (desktop.getAttribute(DESKTOP_ID_ATTRIBUTE) == null) {
+			String number = "0";
+			desktop.setAttribute(DESKTOP_ID_ATTRIBUTE, number);
+		}
+		return null;
+	}
+
+	@Override
+	public String nextPageUuid(Page page) {
+		return null;
+	}
+
+}
