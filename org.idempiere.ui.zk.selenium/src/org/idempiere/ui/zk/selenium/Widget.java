c6c37114a0990aea400193bf525ddcf5f7a5d565
hg <hg@localhost>
2012-12-19 15:03:41 +0100

    Merge with 8f66526fa200f8b42ae66e25fdef0fcc5ee04364

---
+++ org.idempiere.ui.zk.selenium/src/org/idempiere/ui/zk/selenium/Widget.java
@@ -0,0 +1,57 @@
+package org.idempiere.ui.zk.selenium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.internal.WrapsDriver;
+
+public class Widget extends By {
+
+	private String locator;
+
+	public Widget(String locator) {
+		this.locator = locator;
+	}
+	
+	public Widget(WebElement element) {
+		String id = element.getAttribute("id");
+		locator = "#"+id;
+	}
+
+	@Override
+	public List<WebElement> findElements(SearchContext context) {
+		List<WebElement> list = new ArrayList<WebElement>();
+		if (context instanceof WebDriver)
+			list.add(findElement((WebDriver) context));
+		else {
+			WrapsDriver wrapsDriver = (WrapsDriver) context;			
+			list.add(findElement(wrapsDriver.getWrappedDriver()));
+		}
+		return list;
+	}
+
+	private WebElement findElement(WebDriver driver) {
+		JavascriptExecutor executor = (JavascriptExecutor) driver;
+		return (WebElement) executor.executeScript("return zk('"+locator+"').$().$n();");
+	}
+	
+	public WebElement $n(WebDriver driver, String subName) {
+		JavascriptExecutor executor = (JavascriptExecutor) driver;
+		return (WebElement) executor.executeScript("return zk('"+locator+"').$().$n('"+subName+"');");
+	}
+
+	public void execute(WebDriver driver, String command) {
+		JavascriptExecutor executor = (JavascriptExecutor) driver;
+		executor.executeScript("zk('"+locator+"').$()."+command+";");
+	}
+
+	public Object eval(WebDriver driver, String command) {
+		JavascriptExecutor executor = (JavascriptExecutor) driver;
+		return executor.executeScript("return zk('"+locator+"').$()."+command+";");
+	}
+}
---
+++ org.idempiere.webservices/WEB-INF/src/com/_3e/ADInterface/ADInfo.java
@@ -0,0 +1,16 @@
+package com._3e.ADInterface;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.idempiere.adInterface.x10.DataRow;
+
+public interface ADInfo {
+  public void setParameters( PreparedStatement pstmt, boolean forCount) throws SQLException;
+  //public String getSQLWhere();
+  public String getSQL();
+  public String getSQLCount();
+  
+  public DataRow getColumns();
+  
+}
