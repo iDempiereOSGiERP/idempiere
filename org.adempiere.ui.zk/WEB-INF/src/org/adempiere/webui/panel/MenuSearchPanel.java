0a309b141eb6a7c50287d2cda51b1669f897745f
hengsin <hengsin@localhost>
2013-01-11 11:49:46 +0800

    IDEMPIERE-546 Integrate Apache Felix Web Console. Accessible at osgi/system/console using the administrator login.

---
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/panel/MenuSearchPanel.java
@@ -0,0 +1,82 @@
+/******************************************************************************
+ * Copyright (C) 2012 Elaine Tan                                              *
+ * Copyright (C) 2012 Trek Global
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *****************************************************************************/
+
+package org.adempiere.webui.panel;
+
+import org.zkoss.zk.ui.Component;
+import org.zkoss.zk.ui.event.Event;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.EventQueues;
+import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zul.Checkbox;
+import org.zkoss.zul.Toolbar;
+
+/**
+ * Menu Search Panel
+ * @author Elaine
+ * @date July 31, 2012
+ */
+public class MenuSearchPanel extends AbstractMenuPanel
+{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 5308522340852904168L;
+	
+	private TreeSearchPanel pnlSearch;
+    
+    public MenuSearchPanel(Component parent)
+    {
+    	super(parent);
+    }
+
+	protected void init() 
+	{
+		super.init();
+        pnlSearch.initialise();
+        pnlSearch.cmbSearch.setDraggable("favourite");
+        
+        EventQueues.lookup(MenuTreeFilterPanel.MENU_TREE_FILTER_CHECKED_QUEUE, EventQueues.APPLICATION, true).subscribe(new EventListener<Event>() {
+			public void onEvent(Event event) throws Exception {
+				if (event.getName() == Events.ON_CHECK)
+				{
+					Checkbox chk = (Checkbox) event.getData();
+					if (chk != null)
+					{
+						if ("flatView".equals(chk.getId()))
+							MenuTreeFilterPanel.toggleFlatView(getMenuTree(), chk);
+						else
+							MenuTreeFilterPanel.toggle(getMenuTree(), chk);
+						if (pnlSearch != null)
+							pnlSearch.refreshSearchList();
+						getMenuTree().invalidate();
+					}
+				}
+			}
+		});
+	}
+    
+    protected void initComponents()
+    {
+    	super.initComponents();
+    	
+    	Toolbar toolbar = new Toolbar();
+        toolbar.setMold("panel");
+        this.appendChild(toolbar);
+        
+        pnlSearch = new TreeSearchPanel(getMenuTree());
+        pnlSearch.setSclass("menu-search-panel");
+        toolbar.appendChild(pnlSearch);
+    }
+}
