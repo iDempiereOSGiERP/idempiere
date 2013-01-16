14a8363b43b56cd7ebf3e70916f37df5a3b083fe
globalqss <globalqss@localhost>
2013-01-16 05:39:37 -0800

    IDEMPIERE-530 improve performance to enable attachments and chat buttons IDEMPIERE-554 Chat button broken

---
+++ org.adempiere.base/src/org/adempiere/base/ServiceQuery.java
@@ -0,0 +1,13 @@
+package org.adempiere.base;
+
+import java.util.LinkedHashMap;
+
+/**
+ * A sequence of name value pair filter
+ * @author hengsin
+ *
+ */
+public class ServiceQuery extends LinkedHashMap<String, String> {
+
+	private static final long serialVersionUID = -3624488575106821781L;
+}
