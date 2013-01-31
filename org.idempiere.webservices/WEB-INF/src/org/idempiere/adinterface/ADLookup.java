7f2ffb76f3cf8a240ac561958d1cb62c82ff059a
elainetan <elainetan@localhost>
2013-01-31 16:47:17 +0800

    Ticket #1001569: Move receipts into batch from QS to Idempiere - adjustment to the 'Create From Batch' code design

---
+++ org.idempiere.webservices/WEB-INF/src/org/idempiere/adinterface/ADLookup.java
@@ -0,0 +1,50 @@
+package org.idempiere.adinterface;
+
+import org.compiere.model.Lookup;
+import org.compiere.util.CLogger;
+import org.compiere.util.KeyNamePair;
+import org.compiere.util.ValueNamePair;
+import org.idempiere.adInterface.x10.LookupValue;
+import org.idempiere.adInterface.x10.LookupValues;
+
+/*
+ * ADEMPIERE/COMPIERE
+ * 
+ * zamiana:
+ *   GridField na GridField
+ *   GridField
+ */
+public class ADLookup {
+	
+	protected static CLogger	log = CLogger.getCLogger(ADLookup.class);	
+	
+	
+	public static void fillLookupValues( LookupValues lvs, Lookup lookup, boolean isMandatory, boolean isReadOnly  )
+    {		
+    	    	
+		if(lookup.getSize() == 0)
+			lookup.refresh();
+		Object[] list = lookup.getData(isMandatory, true, !isReadOnly, false).toArray();										
+		
+		for (int i=0; i<list.length; i++)
+		{
+			boolean isNumber = list[0] instanceof KeyNamePair;
+			
+			LookupValue lv = lvs.addNewLv();
+			
+			if (isNumber)
+			{
+				KeyNamePair p = (KeyNamePair)list[i];												
+				lv.setKey( Integer.toString(p.getKey()) );
+				lv.setVal( p.getName() );
+				
+			} else
+			{
+				ValueNamePair p = (ValueNamePair)list[i];
+				lv.setKey( p.getValue() );
+				lv.setVal( p.getName() );
+			}									
+		}    	
+    }
+		
+}
