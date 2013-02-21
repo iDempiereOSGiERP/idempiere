40e1dfdf82441f0654eb94a5221d6bf6d3116d56
elainetan <elainetan@localhost>
2013-02-21 16:51:28 +0800

    Ticket #1002211: Create Help pane for Bizidium - fix the css styling for help panel

---
+++ org.adempiere.ui.zk/WEB-INF/src/org/adempiere/webui/component/EditorBox.java
@@ -0,0 +1,148 @@
+/******************************************************************************
+ * Copyright (C) 2008 Low Heng Sin                                            *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *****************************************************************************/
+package org.adempiere.webui.component;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+import org.adempiere.webui.LayoutUtils;
+import org.zkoss.zk.ui.event.EventListener;
+import org.zkoss.zk.ui.event.Events;
+import org.zkoss.zul.Div;
+import org.zkoss.zul.Hlayout;
+
+/**
+ * @author Low Heng Sin
+ */
+public class EditorBox extends Div {
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = -3152111756471436612L;
+	protected PropertyChangeSupport m_propertyChangeListeners = new PropertyChangeSupport(
+			this);
+	protected Textbox txt;
+	protected Button btn;
+
+	public EditorBox() {
+		initComponents();
+	}
+
+	/**
+	 * @param text
+	 */
+	public EditorBox(String text) {
+		initComponents();
+		setText(text);
+	}
+
+	/**
+	 * @param imageSrc
+	 */
+	public void setButtonImage(String imageSrc) {
+		btn.setImage(imageSrc);
+	}
+
+	private void initComponents() {
+		Hlayout hlayout = new Hlayout();
+		this.appendChild(hlayout);
+		hlayout.setHflex("1");
+		txt = new Textbox();
+		hlayout.appendChild(txt);
+		txt.setHflex("1");
+
+		btn = new Button();
+		btn.setTabindex(-1);
+		btn.setSclass("editor-button");
+		btn.setHflex("0");
+		hlayout.appendChild(btn);
+
+		LayoutUtils.addSclass("editor-box", this);
+	}
+
+	/**
+	 * @return textbox component
+	 */
+	public Textbox getTextbox() {
+		return txt;
+	}
+
+	/**
+	 * @param value
+	 */
+	public void setText(String value) {
+		txt.setText(value);
+	}
+
+	/**
+	 * @return text
+	 */
+	public String getText() {
+		return txt.getText();
+	}
+
+	/**
+	 * @param enabled
+	 */
+	public void setEnabled(boolean enabled) {
+		txt.setReadonly(!enabled);
+		btn.setEnabled(enabled);
+		btn.setVisible(enabled);
+		if (enabled) {
+			btn.setSclass("editor-button");
+			btn.setParent(this.getFirstChild());
+		} else {
+			btn.detach();
+		}
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public boolean isEnabled() {
+		return !txt.isReadonly();
+	}
+
+	/**
+	 * @param evtnm
+	 * @param listener
+	 */
+	public boolean addEventListener(String evtnm, EventListener<?> listener) {
+		if (Events.ON_CLICK.equals(evtnm)) {
+			return btn.addEventListener(evtnm, listener);
+		} else {
+			return txt.addEventListener(evtnm, listener);
+		}
+	}
+
+	/**
+	 * @param l
+	 */
+	public synchronized void addPropertyChangeListener(PropertyChangeListener l) {
+		m_propertyChangeListeners.addPropertyChangeListener(l);
+	}
+
+	/**
+	 * @param tooltiptext
+	 */
+	public void setToolTipText(String tooltiptext) {
+		txt.setTooltiptext(tooltiptext);
+	}
+	
+	/**
+	 * @return Button
+	 */
+	public Button getButton() {
+		return btn;
+	}
+}
