3b07702b33c4c072019939d1a0e03f5aa6980d52
elainetan <elainetan@localhost>
2013-04-26 18:30:35 +0800

    IDEMPIERE-862 Create lines from MPS Lines function is not working - fix deadlock problem

---
+++ org.adempiere.base/src/org/compiere/model/X_AD_PInstance.java
@@ -0,0 +1,254 @@
+/******************************************************************************
+ * Product: iDempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2012 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+/** Generated Model - DO NOT CHANGE */
+package org.compiere.model;
+
+import java.sql.ResultSet;
+import java.util.Properties;
+import org.compiere.util.KeyNamePair;
+
+/** Generated Model for AD_PInstance
+ *  @author iDempiere (generated) 
+ *  @version Release 1.0b - $Id$ */
+public class X_AD_PInstance extends PO implements I_AD_PInstance, I_Persistent 
+{
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 20130425L;
+
+    /** Standard Constructor */
+    public X_AD_PInstance (Properties ctx, int AD_PInstance_ID, String trxName)
+    {
+      super (ctx, AD_PInstance_ID, trxName);
+      /** if (AD_PInstance_ID == 0)
+        {
+			setAD_PInstance_ID (0);
+			setAD_Process_ID (0);
+			setIsProcessing (false);
+			setRecord_ID (0);
+        } */
+    }
+
+    /** Load Constructor */
+    public X_AD_PInstance (Properties ctx, ResultSet rs, String trxName)
+    {
+      super (ctx, rs, trxName);
+    }
+
+    /** AccessLevel
+      * @return 6 - System - Client 
+      */
+    protected int get_AccessLevel()
+    {
+      return accessLevel.intValue();
+    }
+
+    /** Load Meta Data */
+    protected POInfo initPO (Properties ctx)
+    {
+      POInfo poi = POInfo.getPOInfo (ctx, Table_ID, get_TrxName());
+      return poi;
+    }
+
+    public String toString()
+    {
+      StringBuffer sb = new StringBuffer ("X_AD_PInstance[")
+        .append(get_ID()).append("]");
+      return sb.toString();
+    }
+
+	/** Set Process Instance.
+		@param AD_PInstance_ID 
+		Instance of the process
+	  */
+	public void setAD_PInstance_ID (int AD_PInstance_ID)
+	{
+		if (AD_PInstance_ID < 1) 
+			set_ValueNoCheck (COLUMNNAME_AD_PInstance_ID, null);
+		else 
+			set_ValueNoCheck (COLUMNNAME_AD_PInstance_ID, Integer.valueOf(AD_PInstance_ID));
+	}
+
+	/** Get Process Instance.
+		@return Instance of the process
+	  */
+	public int getAD_PInstance_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_AD_PInstance_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+    /** Get Record ID/ColumnName
+        @return ID/ColumnName pair
+      */
+    public KeyNamePair getKeyNamePair() 
+    {
+        return new KeyNamePair(get_ID(), String.valueOf(getAD_PInstance_ID()));
+    }
+
+	/** Set AD_PInstance_UU.
+		@param AD_PInstance_UU AD_PInstance_UU	  */
+	public void setAD_PInstance_UU (String AD_PInstance_UU)
+	{
+		set_Value (COLUMNNAME_AD_PInstance_UU, AD_PInstance_UU);
+	}
+
+	/** Get AD_PInstance_UU.
+		@return AD_PInstance_UU	  */
+	public String getAD_PInstance_UU () 
+	{
+		return (String)get_Value(COLUMNNAME_AD_PInstance_UU);
+	}
+
+	public org.compiere.model.I_AD_Process getAD_Process() throws RuntimeException
+    {
+		return (org.compiere.model.I_AD_Process)MTable.get(getCtx(), org.compiere.model.I_AD_Process.Table_Name)
+			.getPO(getAD_Process_ID(), get_TrxName());	}
+
+	/** Set Process.
+		@param AD_Process_ID 
+		Process or Report
+	  */
+	public void setAD_Process_ID (int AD_Process_ID)
+	{
+		if (AD_Process_ID < 1) 
+			set_Value (COLUMNNAME_AD_Process_ID, null);
+		else 
+			set_Value (COLUMNNAME_AD_Process_ID, Integer.valueOf(AD_Process_ID));
+	}
+
+	/** Get Process.
+		@return Process or Report
+	  */
+	public int getAD_Process_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_AD_Process_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	public org.compiere.model.I_AD_User getAD_User() throws RuntimeException
+    {
+		return (org.compiere.model.I_AD_User)MTable.get(getCtx(), org.compiere.model.I_AD_User.Table_Name)
+			.getPO(getAD_User_ID(), get_TrxName());	}
+
+	/** Set User/Contact.
+		@param AD_User_ID 
+		User within the system - Internal or Business Partner Contact
+	  */
+	public void setAD_User_ID (int AD_User_ID)
+	{
+		if (AD_User_ID < 1) 
+			set_Value (COLUMNNAME_AD_User_ID, null);
+		else 
+			set_Value (COLUMNNAME_AD_User_ID, Integer.valueOf(AD_User_ID));
+	}
+
+	/** Get User/Contact.
+		@return User within the system - Internal or Business Partner Contact
+	  */
+	public int getAD_User_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_AD_User_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Error Msg.
+		@param ErrorMsg Error Msg	  */
+	public void setErrorMsg (String ErrorMsg)
+	{
+		set_Value (COLUMNNAME_ErrorMsg, ErrorMsg);
+	}
+
+	/** Get Error Msg.
+		@return Error Msg	  */
+	public String getErrorMsg () 
+	{
+		return (String)get_Value(COLUMNNAME_ErrorMsg);
+	}
+
+	/** Set Processing.
+		@param IsProcessing Processing	  */
+	public void setIsProcessing (boolean IsProcessing)
+	{
+		set_Value (COLUMNNAME_IsProcessing, Boolean.valueOf(IsProcessing));
+	}
+
+	/** Get Processing.
+		@return Processing	  */
+	public boolean isProcessing () 
+	{
+		Object oo = get_Value(COLUMNNAME_IsProcessing);
+		if (oo != null) 
+		{
+			 if (oo instanceof Boolean) 
+				 return ((Boolean)oo).booleanValue(); 
+			return "Y".equals(oo);
+		}
+		return false;
+	}
+
+	/** Set Record ID.
+		@param Record_ID 
+		Direct internal record ID
+	  */
+	public void setRecord_ID (int Record_ID)
+	{
+		if (Record_ID < 0) 
+			set_ValueNoCheck (COLUMNNAME_Record_ID, null);
+		else 
+			set_ValueNoCheck (COLUMNNAME_Record_ID, Integer.valueOf(Record_ID));
+	}
+
+	/** Get Record ID.
+		@return Direct internal record ID
+	  */
+	public int getRecord_ID () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_Record_ID);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+
+	/** Set Result.
+		@param Result 
+		Result of the action taken
+	  */
+	public void setResult (int Result)
+	{
+		set_Value (COLUMNNAME_Result, Integer.valueOf(Result));
+	}
+
+	/** Get Result.
+		@return Result of the action taken
+	  */
+	public int getResult () 
+	{
+		Integer ii = (Integer)get_Value(COLUMNNAME_Result);
+		if (ii == null)
+			 return 0;
+		return ii.intValue();
+	}
+}
