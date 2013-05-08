2253042dd4f87e116b77c51a7105710911e6587d
hengsin <hengsin@localhost>
2013-05-08 16:23:54 +0800

    IDEMPIERE-724 Zk: Make iDempiere theme more easily customizable. Allow theme to make bigger changes to the appearance of desktop header. Allow theme to change the timeout page.

---
+++ migration/i1.0a-i1.0b/postgresql/201212131724_WS002_WebServicesDefinition.sql
@@ -0,0 +1,34 @@
+-- Jan 30, 2009 7:13:26 PM COT
+-- Web Service Definition and Security
+
+INSERT INTO WS_WebService (AD_Client_ID,AD_Org_ID,Created,CreatedBy,Description,Help,IsActive,Name,Updated,UpdatedBy,Value,WS_WebService_ID) VALUES (0,0,TO_TIMESTAMP('2009-01-30 19:17:43','YYYY-MM-DD HH24:MI:SS'),100,'Contributed by http://www.globalqss.com - implemented security','This web services are very generic - so it''s necessary to configure the security layer in the web services security window to make them work.  You can take a look to GardenWorld samples.','Y','Model Oriented Web Services',TO_TIMESTAMP('2009-01-30 19:17:43','YYYY-MM-DD HH24:MI:SS'),100,'ModelADService',50001)
+;
+
+INSERT INTO WS_WebServiceMethod (AD_Client_ID,AD_Org_ID,Created,CreatedBy,Description,Help,IsActive,Name,Updated,UpdatedBy,Value,WS_WebService_ID,WS_WebServiceMethod_ID) VALUES (0,0,TO_TIMESTAMP('2009-01-30 19:19:03','YYYY-MM-DD HH24:MI:SS'),100,'To invoke corresponding methods of documents when changing document action.','Change DocAction for documents, i.e. Complete a Material Receipt
+WARNING! This web service complete documents not via workflow, so it jump over any approval step considered in document workflow.
+To complete documents using workflow it''s better to use the runProcess web service','Y','Set Document Action',TO_TIMESTAMP('2009-01-30 19:19:03','YYYY-MM-DD HH24:MI:SS'),100,'setDocAction',50001,50021)
+;
+
+INSERT INTO WS_WebServiceMethod (AD_Client_ID,AD_Org_ID,Created,CreatedBy,Description,IsActive,Name,Updated,UpdatedBy,Value,WS_WebService_ID,WS_WebServiceMethod_ID) VALUES (0,0,TO_TIMESTAMP('2009-01-30 19:19:47','YYYY-MM-DD HH24:MI:SS'),100,'Run a process or report - if the process is a document workflow a document action can be configured','Y','Run Process',TO_TIMESTAMP('2009-01-30 19:19:47','YYYY-MM-DD HH24:MI:SS'),100,'runProcess',50001,50022)
+;
+
+INSERT INTO WS_WebServiceMethod (AD_Client_ID,AD_Org_ID,Created,CreatedBy,IsActive,Name,Updated,UpdatedBy,Value,WS_WebService_ID,WS_WebServiceMethod_ID) VALUES (0,0,TO_TIMESTAMP('2009-01-30 19:20:02','YYYY-MM-DD HH24:MI:SS'),100,'Y','Get Reference Lists or Reference Tables',TO_TIMESTAMP('2009-01-30 19:20:02','YYYY-MM-DD HH24:MI:SS'),100,'getList',50001,50023)
+;
+
+INSERT INTO WS_WebServiceMethod (AD_Client_ID,AD_Org_ID,Created,CreatedBy,Description,Help,IsActive,Name,Updated,UpdatedBy,Value,WS_WebService_ID,WS_WebServiceMethod_ID) VALUES (0,0,TO_TIMESTAMP('2009-01-30 19:21:02','YYYY-MM-DD HH24:MI:SS'),100,'Web Service to create data following the persistence model of Adempiere','Web Service to create data following the persistence model of Adempiere, this is calling properly before/afterSave methods - and configured model validators.','Y','Create Data',TO_TIMESTAMP('2009-01-30 19:21:02','YYYY-MM-DD HH24:MI:SS'),100,'createData',50001,50024)
+;
+
+INSERT INTO WS_WebServiceMethod (AD_Client_ID,AD_Org_ID,Created,CreatedBy,Description,Help,IsActive,Name,Updated,UpdatedBy,Value,WS_WebService_ID,WS_WebServiceMethod_ID) VALUES (0,0,TO_TIMESTAMP('2009-01-30 19:21:27','YYYY-MM-DD HH24:MI:SS'),100,'Web Service to modify data following the persistence model of Adempiere','Web Service to modify data following the persistence model of Adempiere, this is calling properly before/afterSave methods - and configured model validators.','Y','Update Data',TO_TIMESTAMP('2009-01-30 19:21:27','YYYY-MM-DD HH24:MI:SS'),100,'updateData',50001,50025)
+;
+
+INSERT INTO WS_WebServiceMethod (AD_Client_ID,AD_Org_ID,Created,CreatedBy,Description,Help,IsActive,Name,Updated,UpdatedBy,Value,WS_WebService_ID,WS_WebServiceMethod_ID) VALUES (0,0,TO_TIMESTAMP('2009-01-30 19:21:52','YYYY-MM-DD HH24:MI:SS'),100,'Web Service to delete data following the persistence model of Adempiere','Web Service to delete data following the persistence model of Adempiere, this is calling properly before/afterDelete methods - and configured model validators.','Y','Delete Data',TO_TIMESTAMP('2009-01-30 19:21:52','YYYY-MM-DD HH24:MI:SS'),100,'deleteData',50001,50026)
+;
+
+INSERT INTO WS_WebServiceMethod (AD_Client_ID,AD_Org_ID,Created,CreatedBy,Description,IsActive,Name,Updated,UpdatedBy,Value,WS_WebService_ID,WS_WebServiceMethod_ID) VALUES (0,0,TO_TIMESTAMP('2009-01-30 19:22:22','YYYY-MM-DD HH24:MI:SS'),100,'Access a record via its ID and return the corresponding data','Y','Read Data',TO_TIMESTAMP('2009-01-30 19:22:22','YYYY-MM-DD HH24:MI:SS'),100,'readData',50001,50027)
+;
+
+INSERT INTO WS_WebServiceMethod (AD_Client_ID,AD_Org_ID,Created,CreatedBy,Description,IsActive,Name,Updated,UpdatedBy,Value,WS_WebService_ID,WS_WebServiceMethod_ID) VALUES (0,0,TO_TIMESTAMP('2009-01-30 19:22:50','YYYY-MM-DD HH24:MI:SS'),100,'Access records via conditions on columns - return data rows','Y','Query Data',TO_TIMESTAMP('2009-01-30 19:22:50','YYYY-MM-DD HH24:MI:SS'),100,'queryData',50001,50028)
+;
+
+SELECT register_migration_script('201212131724_WS002_WebServicesDefinition.sql') FROM dual
+;
---
+++ org.adempiere.ui/src/org/compiere/apps/ALoginRes_hu.java
@@ -0,0 +1,80 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2006 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+package org.compiere.apps;
+
+import java.util.ListResourceBundle;
+
+/**
+ *  Base Resource Bundle.
+ *  If you translate it, make sure that you convert the file to ASCII via
+ *  native2ascii 
+ *  http://java.sun.com/j2se/1.5.0/docs/tooldocs/windows/native2ascii.html
+ *  The non ASCII characters need to be converted to unicode - e.g. \u00ab
+ *  This makes it less readable in the source, but viewable for everyone
+ *
+ * 	@author 	Jorg Janke
+ * 	@version 	$Id: ALoginRes.java,v 1.2 2006/07/30 00:51:27 jjanke Exp $
+ */
+public final class ALoginRes_hu extends ListResourceBundle
+{
+	/** Translation Content     */
+	static final Object[][] contents = new String[][]
+	{
+	{ "Connection",         "Kapcsolat" },
+	{ "Defaults",           "Alap\u00e9rtelmez\u00e9s" },
+	{ "Login",              "iDempiere Bel\u00e9p\u00e9s" },
+	{ "File",               "F\u00e1jl" },
+	{ "Exit",               "Kil\u00e9p\u00e9s" },
+	{ "Help",               "S\u00fag\u00f3" },
+	{ "About",              "N\u00e9vjegy" },
+	{ "Host",               "Szerver" },
+	{ "Database",           "Adatb\u00e1zis" },
+	{ "User",               "Felhaszn\u00e1l\u00f3 ID" },
+	{ "EMail",              "EMail" },
+	{ "EnterUser",          "\u00cdrja be a felhaszn\u00e1l\u00f3 ID-t" },
+	{ "Password",           "Jelsz\u00f3" },
+	{ "EnterPassword",      "\u00cdrja be a jelszav\u00e1t" },
+	{ "Language",           "Nyelv" },
+	{ "SelectLanguage",     "V\u00e1lasszon nyelvet" },
+	{ "Role",               "Szerepk\u00f6r" },
+	{ "Client",             "V\u00e1llalat" },
+	{ "Organization",       "Szervezet" },
+	{ "Date",               "D\u00e1tum" },
+	{ "Warehouse",          "Rakt\u00e1r" },
+	{ "Printer",            "Nyomtat\u00f3" },
+	{ "Connected",          "Csatlakoztatva" },
+	{ "NotConnected",       "Nincs csatlakoztatva" },
+	{ "DatabaseNotFound",   "Az adatb\u00e1zis nem tal\u00e1lhat\u00f3" },
+	{ "UserPwdError",       "A felhaszn\u00e1l\u00f3 vagy jelsz\u00f3 hib\u00e1s" },
+	{ "RoleNotFound",       "A szerepk\u00f6r nem tal\u00e1lhat\u00f3" },
+	{ "Authorized",         "Jogosults\u00e1g ellen\u0151rizve" },
+	{ "Ok",                 "Ok" },
+	{ "Cancel",             "M\u00e9gsem" },
+	{ "VersionConflict",    "Verzi\u00f3 \u00fctk\u00f6z\u00e9s:" },
+	{ "VersionInfo",        "Szerver <> Kliens" },
+	{ "PleaseUpgrade",      "T\u00f6ltse le a program \u00faj verzi\u00f3j\u00e1t a szerverr\u0151l" }
+	};
+
+	/**
+	 *  Get Contents
+	 *  @return context
+	 */
+	public Object[][] getContents()
+	{
+		return contents;
+	}   //  getContents
+}   //  ALoginRes
