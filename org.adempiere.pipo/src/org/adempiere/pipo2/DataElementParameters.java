eb8a110ecfe52e9a4a6b9b88745fcb8bfe0fa183
globalqss <globalqss@localhost>
2013-06-13 13:58:45 -0500

    IDEMPIERE-974 fix typo in message

---
+++ org.adempiere.pipo/src/org/adempiere/pipo2/DataElementParameters.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 2010 Heng Sin Low                							  *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ *****************************************************************************/
+package org.adempiere.pipo2;
+
+/**
+ *
+ * @author hengsin
+ *
+ */
+public interface DataElementParameters {
+	public final static String AD_TABLE_ID = "AD_Table_ID";
+	public final static String SQL_STATEMENT = "SQLStatement";
+}
