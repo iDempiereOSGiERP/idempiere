0b70be0694eb770857f0919bfb634805f08bb102
jcorredor <jcorredor@localhost>
2013-06-19 08:08:43 -0500

    IDEMPIERE-1067 Doc Matched Inv not processed and not posted

---
+++ org.adempiere.base/src/org/idempiere/fa/exceptions/AssetInvoiceWithMixedLines_LRO.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.idempiere.fa.exceptions;
+
+
+/**
+ * @author Teo Sarca, SC ARHIPAC SERVICE SRL
+ *
+ */
+public class AssetInvoiceWithMixedLines_LRO extends AssetException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7477049497754422353L;
+
+	public AssetInvoiceWithMixedLines_LRO() {
+		super("@AssetInvoiceWithMixedLines@");
+	}
+}
