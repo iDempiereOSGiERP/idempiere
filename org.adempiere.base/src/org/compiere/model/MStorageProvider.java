94e0cbd24e4ea3dc06a0403d21a714d72f930e1e
elainetan <elainetan@localhost>
2013-07-05 14:46:21 +0800

    IDEMPIERE-1132 Improve AD_Index; AD_View; foreign keys management

---
+++ org.adempiere.base/src/org/compiere/model/MStorageProvider.java
@@ -0,0 +1,66 @@
+/******************************************************************************
+ * Product: iDempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2012 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software, you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY, without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program, if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ *****************************************************************************/
+package org.compiere.model;
+
+import java.sql.ResultSet;
+import java.util.Properties;
+
+import org.adempiere.base.Service;
+import org.adempiere.base.ServiceQuery;
+import org.adempiere.exceptions.AdempiereException;
+
+public class MStorageProvider extends X_AD_StorageProvider {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5889682671195395536L;
+
+	public MStorageProvider(Properties ctx, int AD_StorageProvider_ID, String trxName) {
+		super(ctx, AD_StorageProvider_ID, trxName);
+	}
+
+	public MStorageProvider(Properties ctx, ResultSet rs, String trxName) {
+		super(ctx, rs, trxName);
+	}
+
+	public IAttachmentStore getAttachmentStore() {
+		ServiceQuery query=new ServiceQuery();
+		String method = this.getMethod();
+		if (method == null)
+			method = "DB";
+		query.put("method", method);
+		IAttachmentStore store = Service.locator().locate(IAttachmentStore.class, query).getService();			
+		if (store == null) {
+			throw new AdempiereException("No attachment storage provider found");
+		}
+		return store;
+	}
+	
+	public IArchiveStore getArchiveStore() {
+		ServiceQuery query=new ServiceQuery();
+		String method = this.getMethod();
+		if (method == null)
+			method = "DB";
+		query.put("method", method);
+		IArchiveStore store = Service.locator().locate(IArchiveStore.class, query).getService();
+		if (store == null) {
+			throw new AdempiereException("No archive storage provider found");
+		}
+		return store;
+	}
+		
+}
