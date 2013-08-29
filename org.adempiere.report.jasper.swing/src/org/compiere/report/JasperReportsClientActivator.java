e32f62d5dbf525ac8e323c683383715e709b593f
hengsin <hengsin@localhost>
2013-08-29 11:25:31 +0800

    IDEMPIERE-1313 Web Client: Reduce space used by collapsed context help pane.

---
+++ org.adempiere.report.jasper.swing/src/org/compiere/report/JasperReportsClientActivator.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package org.compiere.report;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * @author hengsin
+ *
+ */
+public class JasperReportsClientActivator implements BundleActivator {
+
+	/**
+	 * default constructor 
+	 */
+	public JasperReportsClientActivator() {
+		super();
+	}
+
+	@Override
+	public void start(BundleContext arg0) throws Exception {
+	}
+
+	@Override
+	public void stop(BundleContext arg0) throws Exception {
+	}
+
+}
