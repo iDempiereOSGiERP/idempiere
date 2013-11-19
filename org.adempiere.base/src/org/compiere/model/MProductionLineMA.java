a54c7773415d7ff3ba15e6244df9e531ac3bfc5c
hengsin <hengsin@localhost>
2013-11-19 21:15:12 +0800

    Merge release-1.0c

---
+++ org.adempiere.base/src/org/compiere/model/MProductionLineMA.java
@@ -0,0 +1,81 @@
+package org.compiere.model;
+
+import java.math.BigDecimal;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Properties;
+
+import org.compiere.model.X_M_ProductionLineMA;
+import org.compiere.util.Env;
+import org.compiere.util.Util;
+
+public class MProductionLineMA extends X_M_ProductionLineMA {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3935841562652510880L;
+
+	public MProductionLineMA(Properties ctx, int M_ProductionLineMA_ID,
+			String trxName) {
+		super(ctx, M_ProductionLineMA_ID, trxName);
+	}
+
+	public MProductionLineMA(Properties ctx, ResultSet rs, String trxName) {
+		super(ctx, rs, trxName);
+	}
+	
+	/**
+	 * Parent constructor
+	 * @param parent
+	 * @param asi
+	 * @param qty
+	 * @param dateMaterialPolicy
+	 */
+	public MProductionLineMA( MProductionLine parent, int asi, BigDecimal qty,Timestamp dateMaterialPolicy)	{		
+		super(parent.getCtx(),0,parent.get_TrxName());
+		setM_AttributeSetInstance_ID(asi);
+		setM_ProductionLine_ID(parent.get_ID());
+		setMovementQty(qty);
+		setAD_Org_ID(parent.getAD_Org_ID());
+		if (dateMaterialPolicy == null)
+		{
+			if (asi > 0)
+			{
+				MAttributeSetInstance masi = new MAttributeSetInstance(parent.getCtx(), asi, parent.get_TrxName());
+				dateMaterialPolicy = masi.getCreated();
+			}
+			else
+			{
+				dateMaterialPolicy = parent.getM_Production().getMovementDate();
+			}
+		}
+		setDateMaterialPolicy(dateMaterialPolicy);
+	}
+	
+	@Override
+	public void setDateMaterialPolicy(Timestamp DateMaterialPolicy) {
+		if (DateMaterialPolicy != null)
+			DateMaterialPolicy = Util.removeTime(DateMaterialPolicy);
+		super.setDateMaterialPolicy(DateMaterialPolicy);
+	}
+	
+	public static MProductionLineMA get( MProductionLine parent, int asi, Timestamp dateMPolicy )  {
+		String where = " M_ProductionLine_ID = ? AND M_AttributeSetInstance_ID = ? ";
+		if(dateMPolicy==null){
+			dateMPolicy = new Timestamp(new Date().getTime());
+		}
+		where = where + "AND DateMaterialPolicy = trunc(cast(? as date))";
+		
+		MProductionLineMA lineMA = MTable.get(parent.getCtx(), MProductionLineMA.Table_Name).createQuery(where, parent.get_TrxName())
+		.setParameters(parent.getM_ProductionLine_ID(), asi,dateMPolicy).first();
+		
+		if (lineMA != null)
+			return lineMA;
+		else
+			return new MProductionLineMA( parent,
+				asi,
+				Env.ZERO,dateMPolicy);
+	}
+
+}
